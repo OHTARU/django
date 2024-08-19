c0aea098b778a746f293d77c954f426749bf7be2
hotaru <dusckd4948@naver.com>
2024-08-19 21:20:26 +0900

    SignUp / Login / Logout Page

---
+++ accounts/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django.contrib.auth import authenticate
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'gender']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+        user = authenticate(email=email, password=password)
+        if not user:
+            raise forms.ValidationError("Invalid login credentials")
+        return self.cleaned_data
